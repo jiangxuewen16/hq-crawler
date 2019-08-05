4b1c81e8878fc0d6ac59b83f5fda874343190abb
DESKTOP-094KS21\jiang <445251692@qq.com>
2019-08-05 22:01:21 +0800

    注解式路由

---
+++ core/lib/helper.py
@@ -0,0 +1,39 @@
+import importlib
+import os
+import pkgutil
+import sys
+
+"""
+获取当前模块
+"""
+
+
+def get_module():
+    def main_module_name():
+        mod = sys.modules['__main__']
+        file = getattr(mod, '__file__', None)
+        return file and os.path.splitext(os.path.basename(file))[0]
+
+    def modname(fvars):
+
+        file, name = fvars.get('__file__'), fvars.get('__name__')
+        if file is None or name is None:
+            return None
+
+        if name == '__main__':
+            name = main_module_name()
+        return name
+
+    return modname(globals())
+
+
+def auto_import_module(moduleName, maxLevel=2):
+    maxLevel -= 1
+    if maxLevel <= 0:
+        return
+    module = importlib.import_module(moduleName)
+    for filefiner, name, ispkg in pkgutil.walk_packages(module.__path__):
+        if ispkg:
+            auto_import_module(moduleName + '.' + name)
+        importlib.import_module(moduleName + '.' + name)
+        print("{0} name: {1:12}, is_sub_package: {2}".format(filefiner, name, ispkg))
