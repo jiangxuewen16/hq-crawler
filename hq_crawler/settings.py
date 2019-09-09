1dad7607c817da23e245424a5f39a6a3df1ab3e2
DESKTOP-094KS21\jiang <445251692@qq.com>
2019-09-09 09:11:50 +0800

    更新架构

--- hq_crawler/settings.py
+++ hq_crawler/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'f2#y!1$b2_(x&fe3-du_&5jml#x1gcb%#w12)=rjofxd-wucp1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['11.75.1.20', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
