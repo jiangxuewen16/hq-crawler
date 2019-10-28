23622d0d02db81c4f73838706b7099aeadc3314a
陈文瑜 <250090007@qq.com>
2019-10-28 17:28:18 +0800

    hqlx订单接口测试

--- hq_crawler/settings.py
+++ hq_crawler/settings.py
@@ -112,8 +112,8 @@ WSGI_APPLICATION = 'hq_crawler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.mysql',
         # 'NAME': 'hq_crawler',
         # 'USER': 'root',
