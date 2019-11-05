9809d42b4e3dc468d945e0e94feddd75fdd56bea
陈文瑜 <250090007@qq.com>
2019-11-05 17:34:40 +0800

    口碑接口

--- hq_crawler/__init__.py
+++ hq_crawler/__init__.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import
+
+import os
+
 import pymysql
 
 from core.common.helper import auto_import_module
 from .celery import app as celery_app
+
 
 # 这里放每个应用的视图包，自动加载，主要用于自定路径路由注册
 auto_import_module('apps.api.views')  # view包，业务代码写到此包中
