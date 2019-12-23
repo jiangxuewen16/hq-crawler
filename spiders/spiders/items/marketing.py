fe74a5ae655bbfa3e15642f0323275700da79f9c
jiangxuewen <445251692@qq.com>
2019-12-23 17:08:32 +0800

    营销推广表

--- spiders/spiders/items/marketing.py
+++ spiders/spiders/items/marketing.py
@@ -27,13 +27,21 @@ class Account(BaseItem, mongoengine.Document):
     id = ObjectId()  # 必须定义
 
     type = mongoengine.StringField(max_length=10)
-    platform = mongoengine.StringField(max_length=50)
-    name = mongoengine.StringField()
+    platform = mongoengine.StringField()
+    account_name = mongoengine.StringField()  # 账号名称
 
-    recommend_num = mongoengine.IntField()
-    read_num = mongoengine.IntField()
+    exposure_num = mongoengine.IntField()  # 曝光量（推荐量 + 阅读量）
+    recommend_num = mongoengine.IntField()  # 推荐量
+    read_num = mongoengine.IntField()  # 阅读量（小程序访问人数）
     follow_num = mongoengine.IntField()  # 关注（粉丝）
-    income = mongoengine.DecimalField()  # 收入
+    total_income = mongoengine.DecimalField()  # 总收入
+    drawing = mongoengine.DecimalField()  # 总提现
+    balance = mongoengine.DecimalField()  # 总余额（实时）
+
+    account_home = mongoengine.StringField()  # 个人中心地址
+    authorization_information = mongoengine.StringField()  # 授权信息（用于爬虫）
+
+    is_enable = mongoengine.BooleanField()  # 是否启用
 
     create_at = mongoengine.DateTimeField(null=True)
     update_at = mongoengine.DateTimeField(null=True)
@@ -48,10 +56,17 @@ class Article(BaseItem, mongoengine.Document):
     pk = 1  # 必须定义
     id = ObjectId()  # 必须定义
 
-    recommend_num = mongoengine.IntField()
-    read_num = mongoengine.IntField()
+    platform = mongoengine.StringField()  # 平台类型
+    account_id = mongoengine.ObjectIdField()  # 账号id
+    account_name = mongoengine.StringField()  # 账号名称
+    admin_id = mongoengine.ObjectIdField()  # 管理者id
+    admin_name = mongoengine.StringField()  # 管理者名字
+
+    recommend_num = mongoengine.IntField()  # 推荐量
+    read_num = mongoengine.IntField()  # 阅读量
     forward_num = mongoengine.IntField()  # 转发
     like_num = mongoengine.IntField()  # 点赞
+    comment_num = mongoengine.IntField()  # 评论量
 
     content = mongoengine.StringField()
 
@@ -68,11 +83,28 @@ class DailyReport(BaseItem, mongoengine.Document):
     pk = 1  # 必须定义
     id = ObjectId()  # 必须定义
 
+    platform = mongoengine.StringField()  # 平台类型
     account_id = mongoengine.ObjectIdField()  # 账号id
     account_name = mongoengine.StringField()  # 账号名称
-    recommend_num = mongoengine.IntField()
-    read_num = mongoengine.IntField()
-    follow_num = mongoengine.IntField()  # 关注（粉丝）
-    unfollow_num = mongoengine.IntField()  # 取消关注（粉丝）
-    income = mongoengine.DecimalField()  # 收入
+    admin_id = mongoengine.ObjectIdField()  # 管理者id
+    admin_name = mongoengine.StringField()  # 管理者名字
 
+    exposure_num = mongoengine.IntField()  # 曝光量（推荐量 + 阅读量）
+    recommend_num = mongoengine.IntField()  # 推荐量
+    read_num = mongoengine.IntField()  # 阅读量（小程序访问人数）
+    follow_num = mongoengine.IntField()  # 当日关注（粉丝）
+    unfollow_num = mongoengine.IntField()  # 当日取消关注（粉丝）
+    add_follow_num = mongoengine.IntField()  # 净增关注（粉丝）[当日关注 - 当日取消关注] [小程序新增用户]
+    total_follow_num = mongoengine.IntField()  # 总关注（粉丝）[小程序累计用户]
+
+    # 文章曝光
+    forward_num = mongoengine.IntField()  # 转发
+    like_num = mongoengine.IntField()  # 点赞
+    comment_num = mongoengine.IntField()  # 评论量
+
+    income = mongoengine.DecimalField()  # 收入
+    drawing = mongoengine.DecimalField()  # 提现
+    balance = mongoengine.DecimalField()  # 余额（总金额）
+
+    create_at = mongoengine.DateTimeField(null=True)
+    update_at = mongoengine.DateTimeField(null=True)
