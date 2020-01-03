48b43ce5cbb667bde39d1d01444a6c94ed5a8a2e
445251692@qq.com <445251692@qq.com>
2020-01-03 10:37:46 +0800

    优化爬虫架构

--- apps/api/views/home.py
+++ apps/api/views/home.py
@@ -1,10 +1,8 @@
 from django.http import FileResponse
 
-from apps.scheduler.task import crawler
-from core.common.service_code import ServiceCode
+from core.common.helper import get_scrapyd_cli
 from core.lib.view import BaseView
 from core.lib.route import Route
-from apps.scheduler.app import add
 
 
 @Route.route(path='api/home/')
@@ -18,7 +16,22 @@ class Home(BaseView):
 
     @Route.route(path='home/1')
     def home(self):
-        crawler.spot_comment()
+        # crawler.spot_comment()
+
+        jobid = get_scrapyd_cli().schedule('spiders', 'ctrip_comment')
+        print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+        jobid = get_scrapyd_cli().schedule('spiders', 'lvmama_comment')
+        print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+        jobid = get_scrapyd_cli().schedule('spiders', 'mafengwo_comment')
+        print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+        jobid = get_scrapyd_cli().schedule('spiders', 'meituan_comment')
+        print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+        jobid = get_scrapyd_cli().schedule('spiders', 'fliggy_comment')
+        print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+        jobid = get_scrapyd_cli().schedule('spiders', 'qunar_comment')
+        print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+        jobid = get_scrapyd_cli().schedule('spiders', 'ly_comment')
+        print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
         return self.success({})
 
     @Route.route(path='home')
