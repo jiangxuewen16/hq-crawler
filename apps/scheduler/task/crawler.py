7252a8c77961be338e01cae4c2e9661ac300d35c
445251692@qq.com <445251692@qq.com>
2020-01-28 13:12:56 +0800

    调试定时任务

--- apps/scheduler/task/crawler.py
+++ apps/scheduler/task/crawler.py
@@ -3,6 +3,7 @@ from django_apscheduler.jobstores import register_job
 from core.common.helper import get_scrapyd_cli
 from core.lib.task import scheduler
 from apps.api.model.exception import ExcLog
+from django.utils.autoreload import logger
 
 """
 设置定时任务，选择方式为interval，时间间隔为10s
@@ -14,39 +15,39 @@ from apps.api.model.exception import ExcLog
 # @register_job(scheduler, "cron", hour='02', minute='10', id='spot_info')
 # def spot_info():
 #     jobid = get_scrapyd_cli().schedule('spiders', 'ctrip_spot')
-#     print('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
+#     logger.info('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
 #     jobid = get_scrapyd_cli().schedule('spiders', 'lvmama_spot')
-#     print('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
+#     logger.info('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
 #     jobid = get_scrapyd_cli().schedule('spiders', 'mafengwo_spot')
-#     print('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
+#     logger.info('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
 #     jobid = get_scrapyd_cli().schedule('spiders', 'meituan_spot')
-#     print('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
+#     logger.info('=' * 30, '爬虫定时任务:::', '景区信息:::', jobid)
 
 
 @register_job(scheduler, "cron", hour='02', minute='10', id='spot_comment')
 def spot_comment():
     jobid = get_scrapyd_cli().schedule('spiders', 'ctrip_comment')
-    print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+    logger.info('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
     jobid = get_scrapyd_cli().schedule('spiders', 'lvmama_comment')
-    print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+    logger.info('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
     jobid = get_scrapyd_cli().schedule('spiders', 'mafengwo_comment')
-    print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+    logger.info('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
     jobid = get_scrapyd_cli().schedule('spiders', 'meituan_comment')
-    print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+    logger.info('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
     jobid = get_scrapyd_cli().schedule('spiders', 'fliggy_comment')
-    print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+    logger.info('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
     jobid = get_scrapyd_cli().schedule('spiders', 'qunar_comment')
-    print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+    logger.info('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
     jobid = get_scrapyd_cli().schedule('spiders', 'ly_comment')
-    print('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
+    logger.info('=' * 30, '爬虫定时任务:::', '景区评论:::', ':::', jobid)
 
 
 # 订单接口检测 每2秒执行一次
 # @register_job(scheduler, "interval", seconds=5 * 60 * 60)
 # def order_check():
 #     jobid = get_scrapyd_cli().schedule('spiders', 'hqlx_order')
-#     print('=' * 30, 'test:::', 'test:::', jobid)
+#     logger.info('=' * 30, 'test:::', 'test:::', jobid)
 
 @register_job(scheduler, "interval", seconds=5)
 def order_check():
-    print('=' * 30, 'test:::', 'test:::', "测试定时任务")
+    logger.info('=' * 30, 'test:::', 'test:::', "测试定时任务")
