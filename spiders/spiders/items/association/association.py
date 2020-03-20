0fc14b782095452f5e7030cf4fa34bac7587dba8
shawn <csk_shawn@163.com>
2020-03-20 14:28:47 +0800

    1.添加wetool爬虫字段

--- spiders/spiders/items/association/association.py
+++ spiders/spiders/items/association/association.py
@@ -7,10 +7,14 @@ class Association(BaseItem, mongoengine.Document):
     pk = 1  # 必须定义
     id = ObjectId()  # 必须定义
 
-    team_leader_id = mongoengine.IntField()  # 团长ID
-    team_leader_name = mongoengine.StringField()  # 团长姓名
-    team_leader_tel = mongoengine.StringField()  # 团长手机号
-    charger_name = mongoengine.StringField()  # 负责人
-
+    team_leader_id = mongoengine.IntField()             # 团长ID
+    team_leader_name = mongoengine.StringField()        # 团长姓名
+    team_leader_tel = mongoengine.StringField()         # 团长手机号
+    charger_name = mongoengine.StringField()            # 负责人
+    chat_room_id = mongoengine.StringField()            # 群id
+    chat_room_owner_wxid = mongoengine.StringField()    # 群所属人id
+    chat_room_member_count = mongoengine.IntField()     # 群人数
+    chat_room_nickname = mongoengine.StringField()      # 群名称
+    chat_room_avatar = mongoengine.StringField()        # 群头像
     create_at = mongoengine.DateTimeField()  # 创建时间
     update_at = mongoengine.DateTimeField()  # 更新时间
