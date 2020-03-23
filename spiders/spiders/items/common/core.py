d0968f52b91c146e0a4effd0d5d7cef2084989c7
蒋雪文 <445251692@qq.com>
2020-03-23 10:14:00 +0800

    添加distinctId

--- spiders/spiders/items/common/core.py
+++ spiders/spiders/items/common/core.py
@@ -2,6 +2,7 @@ from scrapy.item import BaseItem
 
 
 class BaseData(BaseItem):
+    distinctId: str = '1740009e-5946-44c4-acd7-e07af010a35'
     event: str = ''
     data: dict = {}
     create_at: str = ''
