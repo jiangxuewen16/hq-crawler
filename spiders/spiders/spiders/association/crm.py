f82c34cea0f58f782456900854b208d2c5f74da3
陈文瑜 <250090007@qq.com>
2020-03-25 15:23:47 +0800

    crm 测试

--- spiders/spiders/spiders/association/crm.py
+++ spiders/spiders/spiders/association/crm.py
@@ -1,14 +1,16 @@
 import json
-from urllib import parse
 
 import scrapy
-from pykafka import KafkaClient
 
 
 class CrmSpider(scrapy.Spider):
     name = "crm"
     allowed_domains = ['crmapi.superboss.cc']
-    start_urls = ['http://crmapi.superboss.cc/oapi/corp/corp_access_token/get.json']
+    start_urls = [
+        'http://crmapi.superboss.cc/oapi/corp/corp_access_token/get.json',
+        'http://crmapi.superboss.cc/oapi/corp/v1/data/list.json'
+
+    ]
     request_cookie = ''
     CORP_ID = 'ding36d91e596177829935c2f4657eb6378f'
     APP_ID = '27d1f87c661c4493abe3bb53195ec66c'
@@ -23,9 +25,40 @@ class CrmSpider(scrapy.Spider):
         headers = {
             'Content-Type': 'application/json'
         }
-        yield scrapy.FormRequest(url=url, body=json.dumps(post_data), method='POST', headers=headers)
+        yield scrapy.FormRequest(url=url, body=json.dumps(post_data), method='POST', headers=headers,
+                                 callback=self.parse)
 
     def parse(self, response):
         response_str = response.body.decode('utf-8')
         json_data = json.loads(response_str)
+        print(json_data['result'])
+        print(json_data)
+        url = self.start_urls[1]
+        post_data = {
+            # "corpAccessToken": json_data['data']['corpAccessToken'],
+            "corpAccessToken": "caaac76ac9bbec7a4db09aac0b9977d2c3524c",
+            "corpId": self.CORP_ID,
+            "deployId": "customer",
+            "conditionConfig": [{
+                "conditions": [
+                    {
+                        "comparison": "CONTAIN",
+                        "deployId": "customer",
+                        "domType": "INPUT"
+                    }
+                ]
+            }],
+            "page": 1,
+            "pageSize": 2
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        yield scrapy.FormRequest(url=url, body=json.dumps(post_data), method='POST', headers=headers,
+                                 callback=self.parse_data)
+
+    def parse_data(self, response):
+        response_str = response.body.decode('utf-8')
+        json_data = json.loads(response_str)
+        print(json_data['result'])
         print(json_data)
