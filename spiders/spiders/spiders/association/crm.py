9062ea6407b7e747a27fe0f24b10bf5e63409ceb
陈文瑜 <250090007@qq.com>
2020-03-26 16:10:34 +0800

    crm DEGUG

--- spiders/spiders/spiders/association/crm.py
+++ spiders/spiders/spiders/association/crm.py
@@ -20,6 +20,12 @@ class CrmSpider(scrapy.Spider):
     APP_SECRET = '13ABCDD4D8E6B0F812074B42E8A64ACD'
     CORE_ACCESS_TOKEN = 'b99dee9141d63559f761da5d8ef0ce9f7b74f0'
 
+    def get_param(self, param, in_name, default):
+        if in_name in param and param[in_name]:
+            return param[in_name]
+        else:
+            return default
+
     def start_requests(self):
         url = self.start_urls[0]
         post_data = {'corpId': self.CORP_ID,
@@ -34,11 +40,12 @@ class CrmSpider(scrapy.Spider):
     def parse(self, response):
         response_str = response.body.decode('utf-8')
         json_data = json.loads(response_str)
-        print(json_data['data']['data']['corpAccessToken'])
         print(json_data)
+        print(json_data['data']['corpAccessToken'])
+
         url = self.start_urls[1]
         post_data = {
-            "corpAccessToken": json_data['data']['data']['corpAccessToken'],
+            "corpAccessToken": json_data['data']['corpAccessToken'],
             # "corpAccessToken": "caaac76ac9bbec7a4db09aac0b9977d2c3524c",
             "corpId": self.CORP_ID,
             "deployId": "customer",
@@ -59,18 +66,18 @@ class CrmSpider(scrapy.Spider):
         }
         yield scrapy.FormRequest(url=url, body=json.dumps(post_data), method='POST', headers=headers,
                                  callback=self.parse_count,
-                                 meta={'corpAccessToken': json_data['data']['data']['corpAccessToken']})
+                                 meta={'corpAccessToken': json_data['data']['corpAccessToken']})
 
     def parse_count(self, response):
         response_str = response.body.decode('utf-8')
         json_data = json.loads(response_str)
-        print(json_data['result'])
-        print(json_data)
+        print(json_data['data']['count'])
+        # print(json_data)
         url = self.start_urls[1]
         headers = {
             'Content-Type': 'application/json'
         }
-        page = math.ceil(json_data['data']['data']['count'] / 100)
+        page = math.ceil(json_data['data']['count'] / 100)
         for page_num in range(1, page + 1):
             post_data = {
                 "corpAccessToken": response.meta['corpAccessToken'],
@@ -94,14 +101,16 @@ class CrmSpider(scrapy.Spider):
     def parse_list(self, response):
         response_str = response.body.decode('utf-8')
         json_data = json.loads(response_str)
-        print(json_data['data']['data']['list'])
-        print(json_data)
+        # print(json_data['data']['list'])
 
-        if 'data' in json_data['data'] and 'list' in json_data['data']['data']:
-            for key, value in enumerate(json_data['data']['tagList']):
-                yield Association.objects(chat_room_id=value['customer_input_4']).update_one(  # 团长群编码
-                    set__team_leader_id=value['customer_input_2'],  # 团长ID(用户ID)
-                    set__team_leader_name=value['custom_nick'],  # 团长姓名
-                    set__team_leader_tel=value['custom_tele'],  # 团长电话
-                    set__charger_name=value['charger_name'],  # 负责人
-                    upsert=True)
+        if 'data' in json_data and 'list' in json_data['data']:
+            for key, value in enumerate(json_data['data']['list']):
+                print(self.get_param(param=value, in_name='customer_input_4', default=''))
+                if 'customer_input_4' in value:
+                    yield Association.objects(chat_room_id=value['customer_input_4']).update_one(  # 团长群编码
+                        set__team_leader_id=self.get_param(param=value, in_name='customer_input_2', default=''),
+                        # 团长ID(用户ID)
+                        set__team_leader_name=self.get_param(param=value, in_name='custom_nick', default=''),  # 团长姓名
+                        set__team_leader_tel=self.get_param(param=value, in_name='custom_tele', default=''),  # 团长电话
+                        set__charger_name=self.get_param(param=value, in_name='charger_name', default=''),  # 负责人
+                        upsert=True)
