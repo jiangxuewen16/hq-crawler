48cc1db59ccf2d4844869c8adbdc728c203b1cfc
蒋雪文 <445251692@qq.com>
2020-03-30 21:56:29 +0800

    优化表结构

--- spiders/spiders/items/distributor/distributor.py
+++ spiders/spiders/items/distributor/distributor.py
@@ -7,7 +7,8 @@ class CDistributor(BaseItem, mongoengine.Document):
     pk = 1  # 必须定义
     id = ObjectId()  # 必须定义
 
-    channel_id1 = mongoengine.StringField()  # 渠道ID
+    channel_id = mongoengine.StringField()  # 渠道ID
+    team_group_id = mongoengine.StringField()  # 群编码
     mobile = mongoengine.StringField()  # 手机号
     create_at = mongoengine.StringField()  # 创建时间
     meta = {'strict': False}
