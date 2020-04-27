9dc3af45c46848ca3b108bedeca167d736e78048
陈文瑜 <250090007@qq.com>
2020-04-27 11:17:31 +0800

    kafka_info

--- apps/api/views/pub_opinion/kafka_info.py
+++ apps/api/views/pub_opinion/kafka_info.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse, HttpRequest
+import json
+
+import requests
 
 from core.lib.route import Route
 from core.lib.view import BaseView
 
 
-@Route.route(path='api/kafka/info/opinion')
+@Route.route(path='api/kafka')
 class PublicOpinion(BaseView):
 
     # 0简单示例（corpAccessToken）
-    @Route.route(path='/index')
+    @Route.route(path='/connector/plugins')
     # 接收请求数据
     def search(self):
-        host = self.request
-        # request.encoding = 'utf-8'
-        # if 'q' in request and request.GET['q']:
-        #     message = '你搜索的内容为: ' + request.GET['q']
-        # else:
-        #     message = '你提交了空表单'
-        return HttpResponse(self.method)
+        login_url = 'http://172.18.54.27:8089/connector-plugins'
+        login_headers = {
+            'Content-Type': 'application/json'
+        }
+        if self.method == 'GET':
+            r = requests.get(url=login_url, headers=login_headers)
+            result = r.json()
+        return self.success(result)
+
+    @Route.route(path='/connectors')
+    # 接收请求数据
+    def connectors(self):
+        login_url = 'http://172.18.54.27:8089/' + self.request_param['url']
+        print(login_url)
+        login_headers = {
+            'Content-Type': 'application/json'
+        }
+        if self.method == 'POST':
+            login_payload = self.request_param['data']
+            r = requests.post(url=login_url, headers=login_headers, data=json.dumps(login_payload))
+            result = r.json()
+        return self.success(result)
