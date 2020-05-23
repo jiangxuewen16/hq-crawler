f0366d495c8ed20dfe9c455bcc7709a9ab744e56
陈文瑜 <250090007@qq.com>
2020-05-23 10:40:18 +0800

    production

--- spiders/spiders/env/production.py
+++ spiders/spiders/env/production.py
@@ -1,3 +1,3 @@
 import mongoengine
 
-mongoengine.connect('hq_data_cloud', username='hq_data_cloud', password='hqlxhqdatacloud2019', host='mongodb://dds-wz9542ab304a64a42.mongodb.rds.aliyuncs.com:3717', alias='default')  # 连接hq_crawler.mongodb
+mongoengine.connect('hq_data_cloud', host='mongodb://11.75.1.124:27017')
