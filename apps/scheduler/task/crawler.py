c63f050d4050de2900d0472adabe813cf2ed1140
蒋雪文 <445251692@qq.com>
2020-05-25 15:55:14 +0800

    修改美团价格监控的爬虫名称

--- apps/scheduler/task/crawler.py
+++ apps/scheduler/task/crawler.py
@@ -45,7 +45,7 @@ def spot_price():
     jobid = get_scrapyd_cli().schedule('spiders', 'lvmama_spot_price')
     jobid = get_scrapyd_cli().schedule('spiders', 'ly_price')
     jobid = get_scrapyd_cli().schedule('spiders', 'Mafengwo_price')
-    jobid = get_scrapyd_cli().schedule('spiders', 'MeituanPrice')
+    jobid = get_scrapyd_cli().schedule('spiders', 'meituan_price')
     jobid = get_scrapyd_cli().schedule('spiders', 'qunar_price')
     logger.info("==========【价格监控：结束】==========")
 
